7e6ea41200fd1ced6e5b16ade8daa8903615de82
Aditya <adityajain1921@gmail.com>
2021-02-20 18:45:53 -0600

    Run yarn format

--- api/src/api/auth.ts
+++ api/src/api/auth.ts
@@ -8,18 +8,24 @@ const router = express.Router();
 //   request.  The first step in Google authentication will involve
 //   redirecting the user to google.com.  After authorization, Google
 //   will redirect the user back to this application at /auth/google/callback
-router.get('/google',
-  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/plus.login'],
+  }),
+);
 
 // GET /auth/google/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-router.get('/google/callback', 
+router.get(
+  '/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req: Request, res:Response) {
+  function (req: Request, res: Response) {
     res.redirect('/');
-  });
+  },
+);
 
 export default router;
--- api/src/auth/passport.ts
+++ api/src/auth/passport.ts
@@ -1,5 +1,8 @@
 import passport from 'passport';
-import { Strategy as GoogleStrategy, VerifyCallback } from 'passport-google-oauth2';
+import {
+  Strategy as GoogleStrategy,
+  VerifyCallback,
+} from 'passport-google-oauth2';
 
 import User from '../models/user';
 
@@ -7,16 +10,27 @@ import User from '../models/user';
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Google
 //   profile), and invoke a callback with a user object.
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:9000/auth/google/callback"
-  },
-  function(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback) {
-       User.findOrCreate({ googleId: profile.id }, function (err: any, user: any) {
-         return done(err, user);
-       });
-  }
-));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:9000/auth/google/callback',
+    },
+    function (
+      accessToken: string,
+      refreshToken: string,
+      profile: any,
+      done: VerifyCallback,
+    ) {
+      User.findOrCreate(
+        { googleId: profile.id },
+        function (err: any, user: any) {
+          return done(err, user);
+        },
+      );
+    },
+  ),
+);
 
 export default passport;
