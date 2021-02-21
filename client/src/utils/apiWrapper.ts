5dfef9edf56f53a32439573740c79d997ef66254
Polarpi <eugeniachen123@gmail.com>
2021-02-20 18:54:05 -0600

    Allow BASE_URL to be exported from apiWrapper

--- client/src/utils/apiWrapper.ts
+++ client/src/utils/apiWrapper.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 
-const BASE_URL = process.env.REACT_APP_VERCEL_URL
+export const BASE_URL = process.env.REACT_APP_VERCEL_URL
   ? `https://${process.env.REACT_APP_VERCEL_URL}/api`
   : 'http://localhost:9000/api';
 
