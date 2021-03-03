22ad341a963b0ba8513e391c47ce8526d5df0bc8
Aditya <adityajain1921@gmail.com>
2021-03-03 02:07:04 -0600

    Add role to schema

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -1,12 +1,30 @@
 import mongoose, { Document } from 'mongoose';
 
+const roleEnum = {
+  CONTRIBUTOR: "CONTRIBUTOR",
+  STAFF: "STAFF",
+  ADMIN: "ADMIN"
+}
+
 interface User extends Document<any> {
   name: string;
+  role: string;
 }
 
 const User = new mongoose.Schema({
   name: { type: String },
   googleId: { type: String },
+  role: {
+    type: String,
+    enum: [
+      roleEnum.CONTRIBUTOR,
+      roleEnum.STAFF,
+      roleEnum.ADMIN,
+    ],
+    required: true,
+  },
 });
 
 export default mongoose.model<User>('User', User);
+//module.exports = mongoose.model<User>("User", User);
+module.exports.roleEnum = roleEnum;
