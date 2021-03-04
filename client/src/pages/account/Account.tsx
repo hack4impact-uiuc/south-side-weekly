3b338e4731708288e7c25b2dca39f07c17a14a93
Polarpi <eugeniachen123@gmail.com>
2021-03-03 18:37:56 -0600

    Add account page + style sheet

---
+++ client/src/pages/account/Account.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+import Sidebar from '../../components/Sidebar';
+
+import '../../css/Account.css';
+
+function Account(): ReactElement {
+  return <Sidebar firstName="Eugenia" />;
+}
+
+export default Account;
