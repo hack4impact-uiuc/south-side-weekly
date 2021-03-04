8517f21812000759e6ba55a2ab417112bd171f9b
Polarpi <eugeniachen123@gmail.com>
2021-03-03 23:04:12 -0600

    Fix import/exports

--- api/src/api/index.ts
+++ api/src/api/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import users from './users';
 import auth from './auth';
+import { requireContributorStatus } from '../auth/authmiddleware';
 
 const router = express.Router();
 
-router.use('/users', users);
+router.use('/users', requireContributorStatus, users);
 router.use('/auth', auth);
 
 export default router;
--- api/src/auth/authmiddleware.ts
+++ api/src/auth/authmiddleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { roleEnum } from '../models/user';
 import validateRequestForRole from './authvalidators';
 
-const requireContributorStatus = (
+export const requireContributorStatus = (
   req: Request,
   res: Response,
   next: NextFunction,
@@ -17,7 +17,7 @@ const requireContributorStatus = (
   res.status(401).send('You are not authorized (requires contributor status).');
 };
 
-const requireStaffStatus = (
+export const requireStaffStatus = (
   req: Request,
   res: Response,
   next: NextFunction,
@@ -31,7 +31,7 @@ const requireStaffStatus = (
   res.status(401).send('You are not authorized (requires staff status).');
 };
 
-const requireAdminStatus = (
+export const requireAdminStatus = (
   req: Request,
   res: Response,
   next: NextFunction,
@@ -40,10 +40,4 @@ const requireAdminStatus = (
     return next();
   }
   res.status(401).send('You are not authorized (requires admin status).');
-};
-
-module.exports = {
-  requireContributorStatus,
-  requireStaffStatus,
-  requireAdminStatus,
 };
