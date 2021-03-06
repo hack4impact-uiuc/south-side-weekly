618401a864b29ffcf4aa16932602975d30295e59
dyang5200 <dy6@illinois.edu>
2021-03-06 17:18:41 -0600

    Need to pull to get semantic

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement, useEffect, useState } from 'react';
 
 import { getSampleResponse, isError } from '../utils/apiWrapper';
 
-// import PitchDoc from './PitchDoc';
+import PitchDoc from './PitchDoc';
 
 import '../css/Home.css';
 
@@ -25,13 +25,13 @@ function Home(): ReactElement {
   return (
     <>
       {/* Temporary until Onboarding Wizard / real home page is merged so I can look at PitchDoc */}
-      {/* <PitchDoc></PitchDoc> */}
-      <h1>MERN Template</h1>
+      <PitchDoc></PitchDoc>
+      {/* <h1>MERN Template</h1>
       <p>
         Below will tell you if the API is running.
         <br />
         {text}
-      </p>
+      </p> */}
     </>
   );
 }
