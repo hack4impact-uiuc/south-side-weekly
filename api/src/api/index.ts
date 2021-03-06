54569726a5e69b4f4d550f7caeccdcf05a1b3a22
Aditya <adityajain1921@gmail.com>
2021-03-06 15:18:03 -0600

    Test validation

--- api/src/api/index.ts
+++ api/src/api/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import users from './users';
 import auth from './auth';
-import { requireContributorStatus } from '../auth/authmiddleware';
+import { requireContributorStatus, requireStaffStatus, requireAdminStatus } from '../auth/authmiddleware';
 
 const router = express.Router();
 
-router.use('/users', requireContributorStatus, users);
+router.use('/users', requireStaffStatus, users);
 router.use('/auth', auth);
 
 export default router;
