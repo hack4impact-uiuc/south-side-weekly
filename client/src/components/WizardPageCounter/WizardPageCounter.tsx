7024f77f6edbeb0120b26c7eb2b27904a5f51937
Amit Sawhney <66758185+amit-sawhney@users.noreply.github.com>
2021-03-10 21:51:13 -0600

    Onboarding Wizard 🧙  (#26)
    * basic onboarding structure
    
    * Add basic info page to onboarding wizard
    
    * added image to choose role page
    
    * Add adpative page count
    
    * Add documentation to methods
    
    * Add onboard pages and refactor naming
    
    * Add onboard 5 and 6 pages
    
    * Add Onboard7 and fix lint
    
    * Remove Completion Page from counter
    
    * Add calendly page
    
    * Fix staff wizard submit flow
    
    * Refcator select button groups
    
    * Update documentation
    
    * Add clickability to wizard page counter
    
    * Fix merge extraneous characters

---
+++ client/src/components/WizardPageCounter/WizardPageCounter.tsx
@@ -0,0 +1,38 @@
+import React, { FC, ReactElement } from 'react';
+import { Button } from 'semantic-ui-react';
+
+import '../../css/wizard/WizardPageCounter.css';
+
+interface IProps {
+  wizardPages: Array<string>;
+  activePage: string;
+  pageChanger(page: string): void;
+}
+
+/**
+ * Builds the a counter display to represent what page the user is on
+ *
+ * @param {Array<string>} wizardPages an array of strings representing each page of the onboarding wizard
+ * @param {string} activePage the page the user is currently viewing
+ * @param {(page: string): void} pageChanger a function to change the current page
+ */
+const WizardPageCounter: FC<IProps> = ({
+  wizardPages,
+  activePage,
+  pageChanger,
+}): ReactElement => (
+  <div className="page-counter-wrapper">
+    {wizardPages.map((wizardPage, index) => (
+      <Button
+        onClick={() => pageChanger(wizardPage)}
+        className={`page-icon ${wizardPage === activePage ? 'active' : ''}`}
+        circular
+        key={index}
+      >
+        {index + 1}
+      </Button>
+    ))}
+  </div>
+);
+
+export default WizardPageCounter;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 
 import Home from './pages/Home';
+import WizardWrapper from './pages/wizard/WizardWrapper';
+
+import './css/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/join" component={WizardWrapper} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
