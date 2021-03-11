0c880d8115a8c9172010da4c9466e3f88708ae7d
Aditya <adityajain1921@gmail.com>
2021-03-10 21:00:30 -0600

    Add input for basic info

--- client/src/pages/profile/Profile.tsx
+++ client/src/pages/profile/Profile.tsx
@@ -1,7 +1,13 @@
 import React, { ReactElement } from 'react';
+import { Input, Label} from 'semantic-ui-react';
 
 import Sidebar from '../../components/Sidebar';
 import Logo from '../../assets/ssw-form-header.png'
+import Mail from '../../assets/mail.png'
+import Phone from '../../assets/phone.png'
+import Linkedin from '../../assets/linkedin.png'
+import Globe from '../../assets/globe.png'
+import Twitter from '../../assets/twitter.png'
 
 import '../../css/Profile.css';
 
@@ -9,14 +15,146 @@ function Profile(): ReactElement {
   
   return (
   <>
-    <Sidebar />
+    {/*<Sidebar />*/}
     <div className="logo-header">
       <img className="logo" alt="SSW Logo" src={Logo} />
     </div>
     <div className="section">
-      <div className="section-title">
-        <h3>Basic Information</h3>
+      
+
+      <div className="basic-input-section">
+        <div className="section-title">
+          <h3>Basic Information</h3>
+        </div>
+        <div className="input-wrapper">
+            <span className="label">Name:</span>
+            {/*<Label horizontal circular>Name</Label>*/}
+            <Input
+              className="input-field"
+              transparent
+            />
+        </div>
+
+        <div className="input-wrapper">
+            <span className="label">Preferred Name:</span>
+            {/*<Label horizontal circular>Name</Label>*/}
+            <Input
+              className="input-field"
+              transparent
+              //fluid
+            />
+        </div>
+
+        <div className="input-wrapper">
+            <span className="label">Gender:</span>
+            {/*<Label horizontal circular>Name</Label>*/}
+            <Input
+              className="input-field"
+              transparent
+              //fluid
+            />
+        </div>
+
+        <div className="input-wrapper">
+            <span className="label">Pronouns:</span>
+            {/*<Label horizontal circular>Name</Label>*/}
+            <Input
+              className="input-field"
+              transparent
+              //fluid
+            />
+        </div>
+
+        <div className="input-wrapper">
+            <span className="label">Position:</span>
+            {/*<Label horizontal circular>Name</Label>*/}
+            <Input
+              className="input-field"
+              transparent
+              //fluid
+            />
+        </div>
+
+        <div className="input-wrapper">
+            <span className="label">Date Joined:</span>
+            {/*<Label horizontal circular>Name</Label>*/}
+            <Input
+              className="input-field"
+              transparent
+              //fluid
+            />
+        </div>
       </div>
+
+      <div className="social-input-section">
+        <div className="section-title">
+          <h3>Socials/Contact</h3>
+        </div>
+        <div className="social-item-wrapper">
+          
+          <div className="input-wrapper">
+              {/*<Label horizontal circular>Name</Label>*/}
+              <Input
+                className="input-field"
+                transparent
+              />
+          </div>
+        </div>
+
+        <div className="social-item-wrapper">
+          <div className="icon">
+            <img src={Mail} alt="mail"/>
+          </div>
+          <div className="input-wrapper">
+              <Input
+                className="input-field"
+                transparent
+                //fluid
+              />
+          </div>
+        </div>
+
+        <div className="input-wrapper">
+            <span className="label">Gender:</span>
+            {/*<Label horizontal circular>Name</Label>*/}
+            <Input
+              className="input-field"
+              transparent
+              //fluid
+            />
+        </div>
+
+        <div className="input-wrapper">
+            <span className="label">Pronouns:</span>
+            {/*<Label horizontal circular>Name</Label>*/}
+            <Input
+              className="input-field"
+              transparent
+              //fluid
+            />
+        </div>
+
+        <div className="input-wrapper">
+            <span className="label">Position:</span>
+            {/*<Label horizontal circular>Name</Label>*/}
+            <Input
+              className="input-field"
+              transparent
+              //fluid
+            />
+        </div>
+
+        <div className="input-wrapper">
+            <span className="label">Date Joined:</span>
+            {/*<Label horizontal circular>Name</Label>*/}
+            <Input
+              className="input-field"
+              transparent
+              //fluid
+            />
+        </div>
+      </div>
+
     </div>
   </>
   );
