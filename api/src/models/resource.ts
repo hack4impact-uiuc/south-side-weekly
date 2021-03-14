36d8617597b9ecc9fe7aa92a5d64a1c6d46801fa
= <=>
2021-03-14 16:06:22 -0400

    Format code

--- api/src/models/resource.ts
+++ api/src/models/resource.ts
@@ -1,19 +1,19 @@
-import mongoose, {Document, Schema} from 'mongoose';
+import mongoose, { Document, Schema } from 'mongoose';
 /**
  * Interface for a Resource Schema.
  */
 interface IResource extends Document<any> {
-    name: string;
-    link: string;
-    teamRole: [string];
+  name: string;
+  link: string;
+  teamRole: [string];
 }
 /**
  * Mongoose Schema to represent a Resource at South Side Weekly.
  */
 const Resource = new mongoose.Schema({
-    name: { type: String, default: null, required: true },
-    link: { type: String, default: null, required: true },
-    teamRole: [{ type: String, default: null, required: true }],
+  name: { type: String, default: null, required: true },
+  link: { type: String, default: null, required: true },
+  teamRole: [{ type: String, default: null, required: true }],
 });
 export default mongoose.model<IResource>('Resource', Resource);
 export { IResource };
