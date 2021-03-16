687c8ae02d149591d1bedf5294c9327246691646
= <=>
2021-03-15 21:33:56 -0400

    Make teamRoles field plural

--- api/src/models/resource.ts
+++ api/src/models/resource.ts
@@ -5,7 +5,7 @@ import mongoose, { Document } from 'mongoose';
 interface IResource extends Document<any> {
   name: string;
   link: string;
-  teamRole: [string];
+  teamRoles: [string];
 }
 /**
  * Mongoose Schema to represent a Resource at South Side Weekly.
@@ -13,7 +13,7 @@ interface IResource extends Document<any> {
 const Resource = new mongoose.Schema({
   name: { type: String, default: null, required: true },
   link: { type: String, default: null, required: true },
-  teamRole: [{ type: String, default: null, required: true }],
+  teamRoles: [{ type: String, default: null, required: true }],
 });
 export default mongoose.model<IResource>('Resource', Resource);
 export { IResource };
