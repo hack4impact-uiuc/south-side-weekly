30b1959e224e1d306fab0ca4c573e3399f82b1a9
Aditya <adityajain1921@gmail.com>
2021-03-17 17:49:38 -0500

    Add sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,6 +1,12 @@
 import React, { ReactElement } from 'react';
+import Home from '../assets/home.svg'
+import Person from '../assets/person.svg'
+import Pitch from '../assets/pitch.svg'
+import Library from '../assets/library.svg'
+import People from '../assets/people.svg'
 
 import '../css/Sidebar.css';
+
 
 function Sidebar(): ReactElement {
   return (
@@ -10,16 +16,19 @@ function Sidebar(): ReactElement {
       </div>
       <div className="vertical-nav">
         <div className="nav-link">
-          <div className="icon">Home</div>
-        </div>
-        <div className="nav-link active">
-          <div className="icon">Profile</div>
+          <img src={Home} alt="home"/>
         </div>
         <div className="nav-link">
-          <div className="icon">Edit</div>
+          <img src={Person} alt="person"/>
         </div>
         <div className="nav-link">
-          <div className="icon">Stats</div>
+          <img src={Pitch} alt="pitch"/>
+        </div>
+        <div className="nav-link">
+          <img src={Library} alt="library"/>
+        </div>
+        <div className="nav-link">
+          <img src={People} alt="people"/>
         </div>
       </div>
     </div>
