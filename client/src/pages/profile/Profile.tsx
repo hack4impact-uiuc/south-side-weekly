0b221d366e3397e389f494b1af4e7361eaa19a4b
Aditya <adityajain1921@gmail.com>
2021-03-17 03:07:52 -0500

    Organize pfp and masthead and add topics and roles

--- client/src/pages/profile/Profile.tsx
+++ client/src/pages/profile/Profile.tsx
@@ -16,21 +16,36 @@ import '../../css/Profile.css';
 
 function Profile(): ReactElement {
   
+  const currentTeamsButtons = [
+    { value: 'Data', color: '#EF8B8B' },
+    { value: 'Editing', color: '#A5C4F2' },
+    { value: 'Fact-checking', color: '#CFE7C4' },
+    { value: 'Illustration', color: '#BAB9E9' },
+    { value: 'Layout', color: '#F9B893' },
+    { value: 'Photography', color: '#D8ACE8' },
+    { value: 'Radio', color: '#F1D8B0' },
+    { value: 'Visuals', color: '#BFEBE0' },
+    { value: 'Writing', color: '#A9D3E5' },
+  ];
+
+  const currentTeams = [
+    {}
+  ]
+
   return (
   <>
     {/*<Sidebar />*/}
     <div className="logo-header">
       <img className="logo" alt="SSW Logo" src={Logo} />
-    </div>
-    <div className="section">
-      
+    </div>      
 
       <div className="pfp-section">
-      {/*
+      <div className="edit-button">
         <Button
           content="Edit Profile"
           className="edit-profile-button"
-        />*/}
+        />
+      </div>
       <div className="pfp">
         <img src={Pfp} alt="pfp" className="pfp"></img>
       </div>
@@ -182,7 +197,36 @@ function Profile(): ReactElement {
         </div>
       </div>
 
-    </div>
+      <div className="topics-section">
+        <div className="section">
+          <div className="list-title">My Topics</div>
+          <Button
+            className="role-topic-button"
+            content="Cannabis"
+          />
+          <Button
+            className="role-topic-button"
+            content="Fun"
+          />
+        </div>
+
+        <div className="section">
+          <div className="list-title">My Roles</div>
+          <Button
+            className="role-topic-button"
+            content="Cannabis"
+          />
+          <Button
+            className="role-topic-button"
+            content="Fun"
+          />
+        </div>
+      </div>
+
+      
+  
+
+    
   </>
   );
 }
