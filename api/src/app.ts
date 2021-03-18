cf86e3364e4bd8a7a1926aa1494f6124c8aa1a1e
Polarpi <eugeniachen123@gmail.com>
2021-03-17 23:17:03 -0500

    Setup auth in app.ts

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,21 +3,41 @@ import cors from 'cors';
 import express, { Request, Response } from 'express';
 import helmet from 'helmet';
 import logger from 'morgan';
+import cookieSession from 'cookie-session';
+import passport from 'passport';
 import apiRoutes from './api';
 import { errorHandler } from './middleware';
 
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({origin: /localhost:\d{4}/, credentials: true}));
+}
 
 app.use(logger('dev'));
 
 app.use(express.json({ limit: '2.1mb' }));
 app.use(express.urlencoded({ limit: '2.1mb', extended: false }));
 
+// Session support
+const sessionConfig = {
+  secret: process.env.SESS_SECRET,
+  secure: false,
+};
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1);
+  sessionConfig.secure = true;
+}
+app.use(cookieSession(sessionConfig));
+
 // Mongo setup
 require('./utils/mongo-setup');
+
+// Passport setup
+require('./utils/passport-setup');
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 app.use('/api', apiRoutes);
