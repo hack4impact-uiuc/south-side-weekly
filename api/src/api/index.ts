67dadd21d0360cce1c295ea2a6ddafe2c5e2b761
Polarpi <eugeniachen123@gmail.com>
2021-03-17 23:18:26 -0500

    Add auth routes

--- api/src/api/index.ts
+++ api/src/api/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import users from './users';
 import pitch from './pitch';
+import auth from './auth';
 
 const router = express.Router();
 
 router.use('/users', users);
 router.use('/pitch', pitch);
+router.use('/auth', auth);
 
 export default router;
