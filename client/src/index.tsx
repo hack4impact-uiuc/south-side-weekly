6fd09373a8e407b39e82a984c92c71f5b40af1e0
Eugenia Chen <eugeniachen123@gmail.com>
2021-03-23 23:05:54 -0500

    Integrate auth frontend + backend (#43)
    * Add Husky commit hook & test
    
    * testing husky
    
    * Add auth packages to api
    
    * Update packages on client
    
    * Setup auth in app.ts
    
    * Set up Google Passport Strategy
    
    * Add _id to user model
    
    * Add sessionizeUser helper function
    
    * Add auth routes
    
    * Format api files
    
    * Add buildURI api helper function
    
    * Export frontend base url from apiWrapper.ts
    
    * Add relevant enums to client utils
    
    * Add login page
    
    * Use credentials as axios default
    
    * Add logout button on homepage
    
    * Add react-loader-spinner package
    
    * Add loading spinner to Login page
    
    * Add validator function and middleware
    
    * Add auth middleware
    
    * Revert "Update packages on client"
    
    This reverts commit c51cfe4b411af4f2e9c7a759ef501b6473912cce.
    
    * Format Login page files
    
    * Replace secret with keys array
    
    * Add google oauth secrets to test workflow
    
    * Address PR feedback
    
    Co-authored-by: Alice Fang <alicesf2@illinois.edu>
    Co-authored-by: Aditya <adityajain1921@gmail.com>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+import axios from 'axios';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
 import WizardWrapper from './pages/wizard/WizardWrapper';
 import ResourcePage from './pages/ResourcePage';
 
 import './css/index.css';
+
+axios.defaults.withCredentials = true;
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,7 +19,8 @@ ReactDOM.render(
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/join" component={WizardWrapper} />
-        <Route exact path="/resource" component={ResourcePage} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/resources" component={ResourcePage} />
       </Switch>
     </Router>
   </React.StrictMode>,
---
+++ client/src/utils/enums.ts
@@ -0,0 +1,12 @@
+export const rolesEnum = {
+  CONTRIBUTOR: 'CONTRIBUTOR',
+  STAFF: 'STAFF',
+  ADMIN: 'ADMIN',
+  TBD: 'TBD',
+};
+
+export const onboardingStatusEnum = {
+  ONBOARDING_SCHEDULED: 'ONBOARDING_SCHEDULED',
+  STALLED: 'STALLED',
+  ONBOARDED: 'ONBOARDED',
+};
