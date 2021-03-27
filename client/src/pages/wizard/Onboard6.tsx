6ba19e1a7b5cc777eba6fb276471d2b7fff31889
Polarpi <eugeniachen123@gmail.com>
2021-03-27 17:38:38 -0500

    Use enums for race and interests on onboarding

--- client/src/pages/wizard/Onboard6.tsx
+++ client/src/pages/wizard/Onboard6.tsx
@@ -8,6 +8,7 @@ import React, {
 
 import Onboard6SVG from '../../assets/onboard6.svg';
 import { handleSelectGroupArray } from '../../utils/helpers';
+import { interestsEnum } from '../../utils/enums';
 import '../../css/wizard/Onboard6.css';
 import WizardSelectButton from '../../components/WizardSelectButton/WizardSelectButton';
 import { interestsButtons } from '../../utils/constants';
@@ -33,6 +34,44 @@ const Onboard6: FC<IProps> = ({ interests, setInterests }): ReactElement => {
     handleSelectGroupArray(e, interests, setInterests);
   };
 
+  // All of the interests buttons to show
+  const interestsButtons = [
+    { display: 'Cannabis', value: interestsEnum.CANNABIS, color: '#CFE7C4' },
+    { display: 'Education', value: interestsEnum.EDUCATION, color: '#A9D3E5' },
+    {
+      display: 'Food and Land',
+      value: interestsEnum.FOOD_AND_LAND,
+      color: '#BFEBE0',
+    },
+    { display: 'Fun', value: interestsEnum.FUN, color: '#F9B893' },
+    { display: 'Health', value: interestsEnum.HEALTH, color: '#F9B893' },
+    { display: 'Housing', value: interestsEnum.HOUSING, color: '#EF8B8B' },
+    {
+      display: 'Immigration',
+      value: interestsEnum.IMMIGRATION,
+      color: '#D8ACE8',
+    },
+    { display: 'Literature', value: interestsEnum.LIT, color: '#A5C4F2' },
+    { display: 'Music', value: interestsEnum.MUSIC, color: '#BFEBE0' },
+    { display: 'Nature', value: interestsEnum.NATURE, color: '#CFE7C4' },
+    { display: 'Politics', value: interestsEnum.POLITICS, color: '#A5C4F2' },
+    {
+      display: 'Stage and Screen',
+      value: interestsEnum.STAGE_AND_SCREEN,
+      color: '#D8ACE8',
+    },
+    {
+      display: 'Transportation',
+      value: interestsEnum.TRANSPORTATION,
+      color: '#F1D8B0',
+    },
+    {
+      display: 'Visual Arts',
+      value: interestsEnum.VISUAL_ARTS,
+      color: '#BAB9E9',
+    },
+  ];
+
   return (
     <div className="onboard6-wrapper">
       <img className="page-svg" alt="onboard6" src={Onboard6SVG} />
@@ -46,6 +85,7 @@ const Onboard6: FC<IProps> = ({ interests, setInterests }): ReactElement => {
             <WizardSelectButton
               key={interest}
               onClick={handleInterests}
+              buttonText={button.display}
               selectedArray={interests}
               width="150px"
               margin="10px 15px 10px 15px"
