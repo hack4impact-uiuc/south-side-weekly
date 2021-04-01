b0012631998480fccbed2d465a71e2e28a893d3e
Polarpi <eugeniachen123@gmail.com>
2021-03-31 22:12:33 -0500

    Add WizardListTitle component

---
+++ client/src/components/WizardListTitle.tsx
@@ -0,0 +1,19 @@
+import React, {FC, ReactElement} from "react";
+
+import "../css/wizard/WizardListTitle.css"
+
+interface IProps {
+  value: string;
+}
+
+/**
+ * 
+ * @param {string} value the text in the title element 
+ */
+const WizardListTitle: FC<IProps> = ({value}): ReactElement => (
+    <div className="list-title-wrapper">
+      <div className="list-title">{value}</div>
+    </div>
+  )
+
+export default WizardListTitle;
