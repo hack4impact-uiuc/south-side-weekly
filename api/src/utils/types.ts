e4f36b25fcd783e93ded17bec933e66237fa9891
Alice Fang <alicesf2@illinois.edu>
2021-04-05 22:19:37 -0500

    Add common and fix imports

---
+++ api/src/utils/types.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document } from 'mongoose';
+import { SessionUser } from './helpers';
+import { IUser as IUserBase, IPitch as IPitchBase } from 'ssw-common';
+
+/**
+ * Interface for a User Schema.
+ */
+export interface IUser extends Document<any>, IUserBase {}
+
+/**
+ * Interface for a Pitch Schema.
+ */
+export interface IPitch extends Document<any>, IPitchBase {
+  _id: string;
+  pitchAuthor: mongoose.Types.ObjectId;
+}
+
+/**
+ * Modules for express-session.
+ */
+declare module 'Express' {
+  interface Request {
+    user: SessionUser;
+  }
+}
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: SessionUser;
+  }
+}
