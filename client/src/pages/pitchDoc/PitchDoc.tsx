c4dc1310455cece129ca1f2499c92201bfa9a730
Amit Sawhney <amit.sawhney2002@gmail.com>
2021-04-27 20:45:25 -0500

    Add filtering dropdowns

--- client/src/pages/pitchDoc/PitchDoc.tsx
+++ client/src/pages/pitchDoc/PitchDoc.tsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useState, ReactElement } from 'react';
+import React, { useEffect, useState, ReactElement, useMemo } from 'react';
 import { IPitch } from 'ssw-common';
-import { Search } from 'semantic-ui-react';
+import { Dropdown, Search } from 'semantic-ui-react';
 
 import { pages } from '../../utils/enums';
+import {
+  parseArrayToSemanticDropdownOptions,
+  parseSemanticMultiSelectTypes,
+} from '../../utils/helpers';
 import Sidebar from '../../components/Sidebar';
 import { getUnclaimedPitches, isError } from '../../utils/apiWrapper';
 import PitchGrid from '../../components/PitchDoc/PitchGrid';
@@ -11,8 +15,68 @@ import Logo from '../../assets/ssw-form-header.png';
 
 import '../../css/pitchDoc/PitchDoc.css';
 
+interface IFilterKeys {
+  role: string;
+  date: string;
+  interests: string[];
+  status: string;
+}
+
+const initialFilterKeys: IFilterKeys = {
+  role: '',
+  date: '',
+  interests: [],
+  status: '',
+};
+
 function PitchDoc(): ReactElement {
   const [unclaimedPitches, setUnclaimedPitches] = useState<IPitch[]>([]);
+  const [filterKeys, setFilterKeys] = useState<IFilterKeys>(initialFilterKeys);
+
+  const roleOptions = useMemo(
+    () =>
+      parseArrayToSemanticDropdownOptions([
+        'Contributor',
+        'Staff',
+        'Admin',
+        'Tbd',
+      ]),
+    [],
+  );
+
+  const dateOptions = useMemo(
+    () =>
+      parseArrayToSemanticDropdownOptions([
+        'Earliest to Latest',
+        'Latest to Earliest',
+      ]),
+    [],
+  );
+
+  const interestOptions = useMemo(
+    () =>
+      parseArrayToSemanticDropdownOptions([
+        'Cannabis',
+        'Food & Land',
+        'Fun',
+        'Health',
+        'Housing',
+        'Immigration',
+        'Lit',
+        'Music',
+        'Nature',
+        'Politics',
+        'Stage and Screen',
+        'Transportation',
+        'Visual Arts',
+      ]),
+    [],
+  );
+
+  const claimOptios = useMemo(
+    () => parseArrayToSemanticDropdownOptions(['Unclaimed', 'Claimed']),
+    [],
+  );
 
   const getAllUnclaimedPitches = async (): Promise<void> => {
     const resp = await getUnclaimedPitches();
@@ -25,6 +89,37 @@ function PitchDoc(): ReactElement {
   useEffect(() => {
     getAllUnclaimedPitches();
   }, []);
+
+  /**
+   * Updates the filter key state.
+   *
+   * @param key the filter key to update
+   * @param newValue the new value to set the filter key's value to
+   */
+  const updateFilterKeys = (
+    key: keyof IFilterKeys,
+    newValue: string | string[],
+  ): void => {
+    const keys: IFilterKeys = { ...filterKeys };
+
+    switch (key) {
+      case 'role':
+      case 'date':
+      case 'status':
+        keys[key] = `${newValue}`;
+        break;
+      case 'interests':
+        if (Array.isArray(newValue)) {
+          keys[key] = newValue;
+        }
+        break;
+      default:
+        console.error('Invalid filter key');
+        break;
+    }
+
+    setFilterKeys({ ...keys });
+  };
 
   return (
     <>
@@ -42,8 +137,61 @@ function PitchDoc(): ReactElement {
           </div>
 
           <div className="container">
-            <div className="filter-section">
-              <div className="filter-text"> Filter/Sort By: </div>
+            <div className="filters">
+              <h2>Filter by: </h2>
+              <Dropdown
+                className="custom-dropdown"
+                text="Roles"
+                options={roleOptions}
+                scrolling
+                clearable
+                selectOnBlur={false}
+                selectOnNavigation={false}
+                onChange={(e, { value }) =>
+                  updateFilterKeys('role', `${value}`)
+                }
+              />
+              <Dropdown
+                className="custom-dropdown"
+                text="Date Joined"
+                options={dateOptions}
+                scrolling
+                clearable
+                selectOnNavigation={false}
+                selectOnBlur={false}
+                onChange={(e, { value }) =>
+                  updateFilterKeys('date', `${value}`)
+                }
+              />
+              <Dropdown
+                className="custom-dropdown"
+                text="Topics of Interest"
+                options={interestOptions}
+                scrolling
+                multiple
+                clearable
+                selectOnNavigation={false}
+                selectOnBlur={false}
+                onChange={(e, { value }) =>
+                  updateFilterKeys(
+                    'interests',
+                    parseSemanticMultiSelectTypes(value!),
+                  )
+                }
+              />
+              <Dropdown
+                className="custom-dropdown"
+                text="Claim Status"
+                options={claimOptios}
+                scrolling
+                multiple
+                clearable
+                selectOnNavigation={false}
+                selectOnBlur={false}
+                onChange={(e, { value }) =>
+                  updateFilterKeys('status', `${value}`)
+                }
+              />
             </div>
           </div>
         </div>
