d970822a912d1fbe25e65659077d228ba4d33ced
Amit Sawhney <amit.sawhney2002@gmail.com>
2021-07-22 16:59:27 -0500

    Refactor sidebar

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -1,9 +1,9 @@
-import React, { ReactElement, useEffect } from 'react';
+import React, { ReactElement, FC } from 'react';
 import { Link } from 'react-router-dom';
 import { Icon, Image } from 'semantic-ui-react';
 
 import { pages } from '../../utils/enums';
-import './styles.css';
+import './styles.scss';
 import { getUserProfilePic } from '../../utils/helpers';
 import { useAuth } from '../../contexts';
 
@@ -11,28 +11,8 @@ interface IProps {
   currentPage: string;
 }
 
-function Sidebar({ currentPage }: IProps): ReactElement {
+const Sidebar: FC<IProps> = ({ currentPage }): ReactElement => {
   const { user, logout } = useAuth();
-
-  useEffect(() => {
-    switch (currentPage) {
-      case pages.HOME:
-        window.document.getElementById('home')!.classList.add('active');
-        break;
-      case pages.PROFILE:
-        window.document.getElementById('profile')!.classList.add('active');
-        break;
-      case pages.PITCHES:
-        window.document.getElementById('pitches')!.classList.add('active');
-        break;
-      case pages.RESOURCES:
-        window.document.getElementById('resources')!.classList.add('active');
-        break;
-      case pages.USERS:
-        window.document.getElementById('users')!.classList.add('active');
-        break;
-    }
-  });
 
   return (
     <div className="sidebar">
@@ -41,31 +21,48 @@ function Sidebar({ currentPage }: IProps): ReactElement {
       </div>
       <div className="vertical-nav">
         <Link to="/homepage">
-          <div id="home" className="nav-link">
+          <div
+            id="home"
+            className={`nav-link ${currentPage === pages.HOME && 'active'}`}
+          >
             <Icon className="icon" name="home" size="large" />
             <div className="icon-text">Home</div>
           </div>
         </Link>
         <Link to={`/profile/${user._id}`}>
-          <div id="profile" className="nav-link">
+          <div
+            id="profile"
+            className={`nav-link ${currentPage === pages.PROFILE && 'active'}`}
+          >
             <Icon className="icon" name="user" size="large" />
             <div className="icon-text">Profile</div>
           </div>
         </Link>
         <Link to="/pitches">
-          <div id="pitches" className="nav-link">
+          <div
+            id="pitches"
+            className={`nav-link ${currentPage === pages.PITCHES && 'active'}`}
+          >
             <Icon className="icon" name="clipboard" size="large" />
             <div className="icon-text">Pitches</div>
           </div>
         </Link>
         <Link to="/resources">
-          <div id="resources" className="nav-link">
+          <div
+            id="resources"
+            className={`nav-link ${
+              currentPage === pages.RESOURCES && 'active'
+            }`}
+          >
             <Icon className="icon" name="book" size="large" />
             <div className="icon-text">Resources</div>
           </div>
         </Link>
         <Link to="/users">
-          <div id="users" className="nav-link">
+          <div
+            id="users"
+            className={`nav-link ${currentPage === pages.USERS && 'active'}`}
+          >
             <Icon className="icon" name="users" size="large" />
             <div className="icon-text">Users</div>
           </div>
@@ -79,6 +76,6 @@ function Sidebar({ currentPage }: IProps): ReactElement {
       </div>
     </div>
   );
-}
+};
 
 export default Sidebar;
