f9af2731dd30c54a2620fa5bb5b72c2844ad3a16
Amit Sawhney <amit.sawhney2002@gmail.com>
2021-08-17 14:31:13 -0500

    Remove unused assets

--- client/src/components/Wizard/Svg/index.tsx
+++ client/src/components/Wizard/Svg/index.tsx
@@ -4,9 +4,6 @@ import { ImageProps } from 'semantic-ui-react';
 import RolePageSVG from '../../../assets/role-page.svg';
 import BasicInfoSvg from '../../../assets/basic-info.svg';
 import PersonalInfoSvg from '../../../assets/personal-information.svg';
-import InvolvementSVG from '../../../assets/involvement-page.svg';
-import TasksSVG from '../../../assets/tasks.svg';
-import ScheduleSVG from '../../../assets/schedule.svg';
 import { wizardPages } from '../../../utils/enums';
 
 import './styles.scss';
@@ -24,12 +21,6 @@ const WizardSvg: FC<SvgProps> = ({ page, ...rest }): ReactElement => {
         return { src: BasicInfoSvg, alt: 'info-graphic' };
       case wizardPages.ONBOARD_2:
         return { src: PersonalInfoSvg, alt: 'personal info' };
-      case wizardPages.ONBOARD_3:
-        return { src: TasksSVG, alt: 'role page' };
-      case wizardPages.ONBOARD_4:
-        return { src: InvolvementSVG, alt: 'role page' };
-      case wizardPages.ONBOARD_5:
-        return { src: ScheduleSVG, alt: 'role page' };
       default:
         return {};
     }
