588e2d367c57a71609d662b7b0936801c8f55538
Feiyu Wong <feiyucw2@illinois.edu>
2021-09-30 23:37:47 -0500

    started email branch

---
+++ api/src/utils/mailer.ts
@@ -0,0 +1,8 @@
+import {nodemailer} from 'nodemailer';
+
+export const sendMail = async (toEmail:string, content:string) => {
+    let testAccount = await nodemailer.createTestAccount();
+    let transporter = nodemailer.createTransport({
+        
+    })
+}
