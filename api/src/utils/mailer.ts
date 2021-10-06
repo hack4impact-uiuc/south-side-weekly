4f7b8ddc11cb0b7461688ce90a3c0078d28aa28d
Zora Zhang <zorazhang@wirelessprv-10-195-49-106.near.illinois.edu>
2021-10-05 20:33:49 -0500

    mailer setup

--- api/src/utils/mailer.ts
+++ api/src/utils/mailer.ts
@@ -1,8 +1,27 @@
-import {nodemailer} from 'nodemailer';
+import nodemailer from 'nodemailer';
 
-export const sendMail = async (toEmail:string, content:string) => {
+export const pitchReviewMail = async (toEmail:string) => {
     let testAccount = await nodemailer.createTestAccount();
+
     let transporter = nodemailer.createTransport({
-        
-    })
+      service: "gmail",
+        auth: {
+          user: process.env.MAIL_USER, // generated ethereal user
+          pass: process.env.MAIL_PASS // generated ethereal password
+        },
+        tls: {
+          rejectUnauthorized:false
+        }
+      });
+    
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: "h4i.ssw@gmail.com", // sender address
+        to: toEmail, // list of receivers
+        subject: "Hello", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<h1>HOWDY</h1><b>Hello world?</b>", // html body
+      });
+      console.log("Message sent: %s", info.messageId);
 }
+
