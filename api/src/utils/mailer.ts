cac2f61e195343cfbeac1d5e12ff0bf2546bf3e8
Zora Zhang <zorazhang@wirelessprv-10-195-49-106.near.illinois.edu>
2021-10-05 21:17:59 -0500

    automate send mail

--- api/src/utils/mailer.ts
+++ api/src/utils/mailer.ts
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 import nodemailer from 'nodemailer'
 
-export const sendMail = (toEmail:string, subject:string, content:string) : void => {
+export const sendMail = async (toEmail:string, subject:string, content:string) : Promise<string> => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -19,33 +18,5 @@ export const sendMail = (toEmail:string, subject:string, content:string) : void 
         subject: subject, // Subject line
         text: content, // plain text body
     });
+    return "success";
 }
-=======
-import nodemailer from 'nodemailer';
-
-export const pitchReviewMail = async (toEmail:string) => {
-    let testAccount = await nodemailer.createTestAccount();
-
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-        auth: {
-          user: process.env.MAIL_USER, // generated ethereal user
-          pass: process.env.MAIL_PASS // generated ethereal password
-        },
-        tls: {
-          rejectUnauthorized:false
-        }
-      });
-    
-      // send mail with defined transport object
-      let info = await transporter.sendMail({
-        from: "h4i.ssw@gmail.com", // sender address
-        to: toEmail, // list of receivers
-        subject: "Hello", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<h1>HOWDY</h1><b>Hello world?</b>", // html body
-      });
-      console.log("Message sent: %s", info.messageId);
-}
-
->>>>>>> 4f7b8ddc11cb0b7461688ce90a3c0078d28aa28d
