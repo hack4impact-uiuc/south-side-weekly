193806d7680c3613ac79ab78c3b03951f8e6e54e
AndrewLester <alester220@gmail.com>
2021-10-05 20:05:32 -0500

    Update common types and models

--- api/src/models/resource.ts
+++ api/src/models/resource.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document } from 'mongoose';
+import mongoose, { Document, Schema } from 'mongoose';
 import { IResource } from 'ssw-common';
 
 type ResourceSchema = IResource & Document<any>;
@@ -9,6 +9,6 @@ type ResourceSchema = IResource & Document<any>;
 const Resource = new mongoose.Schema({
   name: { type: String, default: null, required: true },
   link: { type: String, default: null, required: true },
-  teamRoles: [{ type: String, default: null, required: true }],
+  teamRoles: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 });
 export default mongoose.model<ResourceSchema>('Resource', Resource);
