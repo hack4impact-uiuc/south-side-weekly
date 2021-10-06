7b92d45237b5340cf2c6b4c8613e229e21ea63d0
Feiyu Wong <feiyucw2@illinois.edu>
2021-10-05 20:34:04 -0500

    wrote send mail function

--- api/src/utils/mailer.ts
+++ api/src/utils/mailer.ts
@@ -1,8 +1,21 @@
 import nodemailer from 'nodemailer'
 
-export const sendMail = async (toEmail:string, content:string) => {
-    let testAccount = await nodemailer.createTestAccount();
-    let transporter = nodemailer.createTransport({
-        
-    })
+export const sendMail = (toEmail:string, subject:string, content:string) : void => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASS
+        }, 
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+    // send mail with defined transport object
+    const info = transporter.sendMail({
+        from: process.env.EMAIL_USERNAME, // sender address
+        to: toEmail, // list of receivers
+        subject: subject, // Subject line
+        text: content, // plain text body
+    });
 }
