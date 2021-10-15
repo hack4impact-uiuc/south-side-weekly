dc2851854e05d0fd596c5d783341ff34ad4a9fb3
Amit Sawhney <66758185+amit-sawhney@users.noreply.github.com>
2021-10-14 22:38:01 -0500

    Add aggregate functions (#129)

--- common/index.d.ts
+++ common/index.d.ts
@@ -28,6 +28,13 @@ export interface IUser {
   interests: string[];
 }
 
+interface IUserAggregate extends IUser {
+  aggregated: {
+      claimedPitches: Partial<IPitch>[],
+      submittedPitches: Partial<IPitch>[],
+  }
+}
+
 /**
  * Interface for a Pitch Schema.
  */
@@ -72,6 +79,21 @@ export interface IPitch {
   similarStories: string[];
   deadline: Date;
   conflictOfInterest: boolean;
+}
+
+interface IPitchAggregate extends IPitch {
+  aggregated: {
+      author: Partial<IUser>,
+      assignmentContributors: {
+          user: Partial<IUser>,
+          teams: string[]
+      }[],
+      pendingContributors: {
+          user: Partial<IUser>,
+          teams: string[]
+      }[],
+      reviewedBy: Partial<IUser>
+  }
 }
 
 /**
