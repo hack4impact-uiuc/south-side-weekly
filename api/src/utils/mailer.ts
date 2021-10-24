a7ab19d499507f3430f941c772fa9fb006290520
Feiyu Wong <feiyucw2@illinois.edu>
2021-10-23 21:56:49 -0500

    logging

--- api/src/utils/mailer.ts
+++ api/src/utils/mailer.ts
@@ -2,25 +2,30 @@ import nodemailer from 'nodemailer';
 import { EmailMessage } from './mailer-templates';
 
 export const sendMail = async (message: EmailMessage): Promise<void> => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
-  // send mail with defined transport object
-  transporter.sendMail(message, (err, info) => {
-    console.log('yoyoyo dis andy')
-    if (err) {
-      console.log('error')
-      console.log(err);
-    } else {
-      console.log('sent?')
-      console.log(`Email sent: ${info.response}`);
-    }
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+    // send mail with defined transport object
+    transporter.sendMail(message, (err, info) => {
+      console.log('yoyoyo dis andy')
+      if (err) {
+        console.log('error')
+        console.log(err);
+      } else {
+        console.log('sent?')
+        console.log(`Email sent: ${info.response}`);
+      }
+    });
+  } catch (Error) {
+    console.log(Error.message)
+  }
+  
 };
