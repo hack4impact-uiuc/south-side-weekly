77497ae05a2ae6d6925c554fac83b9efb5a18bc6
Zora Zhang <zorazhang@Zoras-MacBook-Pro.local>
2021-10-28 20:35:02 -0500

    add field, extract teams from form

--- client/src/components/Modals/EditTeam/index.tsx
+++ client/src/components/Modals/EditTeam/index.tsx
@@ -12,6 +12,11 @@ import Swal from 'sweetalert2';
 
 import './styles.scss';
 
+const TestData: { name: string; color: string }[] = [
+  { name: 'Test1', color: 'blue' },
+  { name: 'Test2', color: 'orange' },
+];
+
 // interface TeamModalProps extends ModalProps {}
 
 const TeamModalTrigger: FC<ButtonProps> = ({ ...rest }): ReactElement => (
@@ -32,13 +37,91 @@ const ConfirmTeam = () => {
     }
   });
 };
+interface NewTeamType {
+  _id: string;
+  name: string;
+  color: string;
+  active: boolean;
+}
+
+const teams: NewTeamType[] = [
+  {
+    _id: '5jfjaskdjfjasdf',
+    name: 'Editing',
+    color: '#E7F2FC',
+    active: true,
+  },
+  {
+    _id: '5jfjaskdjfjasdf',
+    name: 'Writing',
+    color: '#FFECE4',
+    active: true,
+  },
+  {
+    _id: '5jfjaskdjfjasdf',
+    name: 'cat',
+    color: 'F6EEFC',
+    active: true,
+  },
+];
+
+const copy = [...teams.map((t) => ({ ...t }))];
+
+// changedValues
+// newTeams
 
 const TeamModal: FC<ModalProps> = ({ ...rest }): ReactElement => {
   // const { teams } = useTeams();
   const [isOpen, setIsOpen] = useState(false);
+  const [formValues, setFormValues] = useState<NewTeamType[]>([...teams]);
+
+  const addField = () => {
+    setFormValues([
+      ...formValues,
+      { _id: 'NEW', name: '', color: '#3d4f91', active: true },
+    ]);
+  };
+
+  const removeField = (i: number) => {
+    const newFormValues = [...formValues];
+    newFormValues.splice(i, 1);
+    setFormValues(newFormValues);
+  };
+
+  const updateFieldText = (i: number, text: string) => {
+    const formValue = formValues[i];
+    formValue.name = text;
+    setFormValues([...formValues]);
+  };
+
+  const updateFieldColor = (i: number, color: string) => {
+    const formValue = formValues[i];
+    formValue.name = color;
+    setFormValues([...formValues]);
+  };
+
+  const saveForm = (): void => {
+    console.log(copy);
+    const newTeams = formValues.filter((team) => team._id === 'NEW');
+    console.log(newTeams);
+
+    const changedValues: typeof newTeams = [];
+
+    copy.forEach((team, index) => {
+      if (
+        team.name !== formValues[index].name ||
+        team.color !== formValues[index].color
+      ) {
+        changedValues.push(formValues[index]);
+      }
+    });
+
+    console.log(changedValues);
+  };
 
   return (
     <Modal
+      closeIcon
       size="tiny"
       open={isOpen}
       onOpen={() => setIsOpen(true)}
@@ -49,30 +132,31 @@ const TeamModal: FC<ModalProps> = ({ ...rest }): ReactElement => {
     >
       <Modal.Header>Edit Teams</Modal.Header>
       <Modal.Content>
-        <div className="lines">
-        <div className="color-pick">
-          <input type="color" value="#3d4f91" />
-        </div>
-        <Input type="text" defaultValue="Editing" />
-        </div>
-        <div className="lines">
-        <div className="color-pick">
-          <input type="color" value="#3d4f91" />
-        </div>
-        <Input type="text" defaultValue="Visuals" />
-        </div>
-        {/* <Form id="submit-team">
-          {teams.map((team, index) => (
-              <div key={index}>
-                  <Input value={team.color} type="color" />
-                  <Input defaultValue = {team.name} type = "text"/>
+        <Form id="submit-team">
+          {formValues.map((team, index) => (
+            <div key={index} className="lines">
+              <div className="color-pick">
+                <input type="color" value={team.color} />
               </div>
+              <Input
+                type="text"
+                onChange={(e, { value }) => updateFieldText(index, value)}
+                value={team.name}
+                disabled={team.name === 'Editing' || team.name === 'Writing'}
+              />
+              {team._id === 'NEW' && (
+                <Icon
+                  name="minus square outline"
+                  onClick={() => removeField(index)}
+                />
+              )}
+            </div>
           ))}
-          <div>
-              <Icon name="plus" />
-              <p>Add new team</p>
-          </div>
-          </Form> */}
+          <Button className="addTeam" onClick={addField}>
+            <Icon name="plus square outline" />
+            <p>Add a new team</p>
+          </Button>
+        </Form>
       </Modal.Content>
       <Modal.Actions>
         <Button
@@ -80,7 +164,7 @@ const TeamModal: FC<ModalProps> = ({ ...rest }): ReactElement => {
           content="Save"
           form="submit-team"
           className="save-button"
-          onClick={ConfirmTeam}
+          onClick={saveForm}
         />
       </Modal.Actions>
     </Modal>
--- client/src/pages/invalid/index.tsx
+++ client/src/pages/invalid/index.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
 import { Image } from 'semantic-ui-react';
+import TeamModal from '../../components/Modals/EditTeam';
 
 import Error from '../../assets/404-error.png';
 import './styles.scss';
@@ -7,6 +8,7 @@ import './styles.scss';
 const NotFound = (): ReactElement => (
   <div className="not-found-page">
     <Image size="medium" src={Error} alt="Page not found. Error 404" />
+    <TeamModal />
   </div>
 );
 
