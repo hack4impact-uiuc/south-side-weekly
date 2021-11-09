6ec8b43f488d2f42e59973a14bdac8afa49b1aa2
nehakonjeti <nkonjeti@gmail.com>
2021-11-09 01:54:48 -0600

    Add toast to submit pitch modal

--- client/src/components/Modals/SubmitPitch/index.tsx
+++ client/src/components/Modals/SubmitPitch/index.tsx
@@ -1,5 +1,6 @@
 import { isEmpty, isUndefined, toString } from 'lodash';
 import React, { FC, ReactElement, useEffect, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import {
   Button,
   Form,
@@ -8,7 +9,6 @@ import {
   ModalProps,
   Popup,
   Header,
-  Message,
 } from 'semantic-ui-react';
 import { IPitch } from 'ssw-common';
 import Swal from 'sweetalert2';
@@ -24,6 +24,11 @@ const MAX_LENGTH = 300;
 interface SubmitPitchModalProps extends ModalProps {
   callback(): void;
 }
+
+const notify = (): string =>
+  toast.error('Please fill out all the fields before submitting!', {
+    position: 'bottom-right',
+  });
 
 const HelperMessage = (): ReactElement => (
   <>
@@ -74,8 +79,6 @@ const SubmitPitchModal: FC<SubmitPitchModalProps> = ({
   >();
   const [didSubmit, setDidSubmit] = useState(false);
   const [writerChoice, setWriterChoice] = useState<boolean>();
-  const [topicsToast, setTopicsToast] = useState<boolean>();
-
   const { user } = useAuth();
 
   useEffect(() => {
@@ -92,7 +95,7 @@ const SubmitPitchModal: FC<SubmitPitchModalProps> = ({
     setDidSubmit(true);
 
     if (isInvalidForm()) {
-      setTopicsToast(true);
+      notify();
       return;
     }
 
@@ -138,7 +141,7 @@ const SubmitPitchModal: FC<SubmitPitchModalProps> = ({
 
     if (isUndefined(field)) {
       return true;
-    } else if (field instanceof Set && isEmpty(field)) {
+    } else if ((field instanceof Set && isEmpty(field)) || field === '') {
       return true;
     }
 
@@ -146,122 +149,125 @@ const SubmitPitchModal: FC<SubmitPitchModalProps> = ({
   };
 
   return (
-    <Modal
-      trigger={<Button className="default-btn" content="Submit a Pitch" />}
-      open={isOpen}
-      className="submit-modal"
-      onOpen={() => setIsOpen(true)}
-      onClose={() => setIsOpen(false)}
-      {...rest}
-    >
-      <Modal.Header>
-        <span style={{ paddingRight: '10px' }}>Submit a pitch</span>
-        <Popup
-          content={HelperMessage()}
-          trigger={<Icon size="small" name="info circle" />}
-          position="bottom left"
-          wide="very"
-          hoverable
-        />
-      </Modal.Header>
-      <Modal.Content>
-        <Form id="submit-pitch" onSubmit={submitPitch}>
-          <Form.Group widths="equal">
-            <Form.Input
-              required
-              error={isFieldError(title)}
-              label={<h5 className="label">Pitch Title</h5>}
-              value={title}
-              onChange={(e, { value }) => setTitle(value)}
-            />
-          </Form.Group>
-          <Form.Group widths="equal">
-            <Form.Input
-              required
-              error={isFieldError(link)}
-              label={<h5 className="label">Google Doc Link</h5>}
-              value={link}
-              onChange={(e, { value }) => setLink(value)}
-            />
-          </Form.Group>
-          <Form.TextArea
-            required
-            maxLength={MAX_LENGTH}
-            value={description}
-            onChange={(e, { value }) => setDescription(toString(value))}
-            label={<h5 className="label">Description</h5>}
-            error={isFieldError(description)}
+    <div>
+      <Toaster></Toaster>
+      <Modal
+        trigger={<Button className="default-btn" content="Submit a Pitch" />}
+        open={isOpen}
+        className="submit-modal"
+        onOpen={() => setIsOpen(true)}
+        onClose={() => setIsOpen(false)}
+        {...rest}
+      >
+        <Modal.Header>
+          <span style={{ paddingRight: '10px' }}>Submit a pitch</span>
+          <Popup
+            content={HelperMessage()}
+            trigger={<Icon size="small" name="info circle" />}
+            position="bottom left"
+            wide="very"
+            hoverable
           />
-          <h5 className="label-topics">
-            Associated Topics<p className="unbold">(select 1-4 topics) </p>
-          </h5>
-          {topicsToast ? (
-            <div>
-              <Message negative>
-                <p>Please pick up to 4 topics</p>
-              </Message>{' '}
-            </div>
-          ) : (
-            <></>
-          )}
-          <InterestsSelect
-            values={topics}
-            onChange={(values) => {
-              if (values.length <= 4) {
-                setTopics(values.map((item) => item.value));
-              }
-            }}
-          />
-          <h5 className="label">Do you want to be the writer of the story? </h5>
-          <Form.Group inline>
-            <Form.Radio
-              name="writer-choice"
-              label="Yes"
-              onClick={() => setWriterChoice(true)}
-              checked={writerChoice}
-              error={isFieldError(writerChoice)}
+        </Modal.Header>
+        <Modal.Content>
+          <Form id="submit-pitch" onSubmit={submitPitch}>
+            <Form.Group widths="equal">
+              <Form.Input
+                error={isFieldError(title)}
+                label={<h5 className="label">Pitch Title</h5>}
+                value={title}
+                onChange={(e, { value }) => setTitle(value)}
+              />
+            </Form.Group>
+            <Form.Group widths="equal">
+              <Form.Input
+                error={isFieldError(link)}
+                label={<h5 className="label">Google Doc Link</h5>}
+                value={link}
+                onChange={(e, { value }) => setLink(value)}
+              />
+            </Form.Group>
+            <Form.TextArea
+              maxLength={MAX_LENGTH}
+              value={description}
+              onChange={(e, { value }) => setDescription(toString(value))}
+              label={<h5 className="label">Description</h5>}
+              error={isFieldError(description)}
             />
-            <Form.Radio
-              name="writer-choice"
-              label="No, I want someone else to write the story"
-              onClick={() => setWriterChoice(false)}
-              checked={!isUndefined(writerChoice) && !writerChoice}
-              error={isFieldError(writerChoice)}
+            <h5 className="label-topics">
+              Associated Topics<p className="unbold">(select 1-4 topics) </p>
+            </h5>
+            <InterestsSelect
+              values={topics}
+              onChange={(values) => {
+                if (values.length <= 4) {
+                  setTopics(values.map((item) => item.value));
+                }
+              }}
             />
-          </Form.Group>
-          <h5 className="label">Conflict of Interest Disclosure</h5>
-          <p>
-            Are you involved with the events or people covered in your pitch?
-            i.e. do you have a relationship with them as an employee, family
-            member, friend, or donor?
-          </p>
-          <Form.Group inline>
-            <Form.Radio
-              name="interest-conflict"
-              label="Yes"
-              onClick={() => setConflictOfInterest(true)}
-              checked={conflictofInterest}
-              error={isFieldError(conflictofInterest)}
+            {user.role === 'WRITER' ? (
+              <div>
+                {' '}
+                <h5 className="label">
+                  Do you want to be the writer of the story?{' '}
+                </h5>
+                <Form.Group inline>
+                  <Form.Radio
+                    name="writer-choice"
+                    label="Yes"
+                    onClick={() => setWriterChoice(true)}
+                    checked={writerChoice}
+                    error={isFieldError(writerChoice)}
+                  />
+                  <Form.Radio
+                    name="writer-choice"
+                    label="No, I want someone else to write the story"
+                    onClick={() => setWriterChoice(false)}
+                    checked={!isUndefined(writerChoice) && !writerChoice}
+                    error={isFieldError(writerChoice)}
+                  />
+                </Form.Group>{' '}
+              </div>
+            ) : (
+              <></>
+            )}
+
+            <h5 className="label">Conflict of Interest Disclosure</h5>
+            <p>
+              Are you involved with the events or people covered in your pitch?
+              i.e. do you have a relationship with them as an employee, family
+              member, friend, or donor?
+            </p>
+            <Form.Group inline>
+              <Form.Radio
+                name="interest-conflict"
+                label="Yes"
+                onClick={() => setConflictOfInterest(true)}
+                checked={conflictofInterest}
+                error={isFieldError(conflictofInterest)}
+              />
+              <Form.Radio
+                name="interest-conflict"
+                label="No"
+                onClick={() => setConflictOfInterest(false)}
+                checked={
+                  !isUndefined(conflictofInterest) && !conflictofInterest
+                }
+                error={isFieldError(conflictofInterest)}
+              />
+            </Form.Group>
+          </Form>
+          <Modal.Actions>
+            <Button
+              type="submit"
+              content="Submit"
+              positive
+              form="submit-pitch"
             />
-            <Form.Radio
-              name="interest-conflict"
-              label="No"
-              onClick={() => setConflictOfInterest(false)}
-              checked={!isUndefined(conflictofInterest) && !conflictofInterest}
-              error={isFieldError(conflictofInterest)}
-            />
-          </Form.Group>
-        </Form>
-        <Modal.Actions>
-          <Button
-            type="submit"
-            content="Submit pitch for review"
-            positive
-            form="submit-pitch"
-          />
-        </Modal.Actions>
-      </Modal.Content>
-    </Modal>
+          </Modal.Actions>
+        </Modal.Content>
+      </Modal>
+    </div>
   );
 };
 
