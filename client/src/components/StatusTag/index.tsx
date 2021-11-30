157950d8842dc073c4725ce8b75d97c5041e7d9d
Amit Sawhney <amit.sawhney2002@gmail.com>
2021-11-30 17:01:40 -0600

    Fix math

--- client/src/components/StatusTag/index.tsx
+++ client/src/components/StatusTag/index.tsx
@@ -9,27 +9,25 @@ interface StatusTagProps extends LabelProps {
   user: IUser;
 }
 
+const MONTHS_IN_YEAR = 12;
+
 const ACTIVE_PERIOD = 3;
+const RECENTLY_ACTIVE = MONTHS_IN_YEAR;
 
 const StatusTag: FC<StatusTagProps> = ({ user, ...rest }) => {
   const getStatus = (): string => {
     const lastActive = new Date(user.lastActive);
     const now = new Date();
 
-    const threeMonthsAgo = new Date(
-      now.getUTCFullYear(),
-      now.getUTCMonth() - ACTIVE_PERIOD,
-      now.getUTCDate(),
-    );
-    const oneYearAgo = new Date(
-      now.getUTCFullYear() - 1,
-      now.getUTCMonth(),
-      now.getUTCDate(),
-    );
+    let months =
+      (now.getUTCFullYear() - lastActive.getUTCFullYear()) * MONTHS_IN_YEAR;
+    months -= lastActive.getUTCMonth();
+    months += now.getUTCMonth();
+    months = months <= 0 ? 0 : months;
 
-    if (lastActive >= threeMonthsAgo) {
+    if (months <= ACTIVE_PERIOD) {
       return statusEnum.ACTIVE;
-    } else if (lastActive > oneYearAgo) {
+    } else if (months <= RECENTLY_ACTIVE) {
       return statusEnum.RECENTLY_ACTIVE;
     }
 
