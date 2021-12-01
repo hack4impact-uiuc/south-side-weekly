aa2e9060488ddae7fbc637afe55f57043285f082
Ayan Mallik <amallik2@illinois.edu>
2021-11-30 21:07:38 -0600

    Update one pitch with issue status

--- api/src/api/pitchFeedback.ts
+++ api/src/api/pitchFeedback.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { errorWrap } from '../middleware';
 
 import PitchFeedback from '../models/pitchFeedback';
+import Pitch from '../models/pitch';
+import { issueStatusEnum } from '../utils/enums';
 
 const router = express.Router();
 
@@ -17,6 +19,31 @@ router.get(
       result: feedback,
     });
   }),
+);
+
+router.put(
+  '/updateMany',
+  errorWrap(async (req: Request, res: Response) => {
+    await Pitch.updateMany(
+      {},
+      {
+        $set: {
+          issueStatuses: [
+            {
+              issueId: '61994d0c0d49689661aaa182',
+              issueStatus: issueStatusEnum.DEFINITELY_IN,
+            },
+          ],
+        },
+      },
+    );
+    res.status(200).json({
+      success: true,
+      message: 'Feedback created successfully',
+      result: "feedback",
+    });
+  }),
+  
 );
 
 // Creates a new feedback
