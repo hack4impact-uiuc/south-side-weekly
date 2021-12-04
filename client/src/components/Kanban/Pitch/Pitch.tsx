8b632aa30ee84beeea939df2ac7d9c3745bbc6b3
Ayan Mallik <amallik2@illinois.edu>
2021-12-03 22:58:18 -0600

    Integrate edit status

--- client/src/components/Kanban/Pitch/Pitch.tsx
+++ client/src/components/Kanban/Pitch/Pitch.tsx
@@ -4,6 +4,7 @@ import { IPitch } from 'ssw-common';
 import cn from 'classnames';
 
 import './styles.scss';
+import FieldTag from '../../FieldTag';
 
 interface PitchProps extends CardProps {
   pitch: IPitch;
@@ -12,7 +13,10 @@ interface PitchProps extends CardProps {
 const Pitch: FC<PitchProps> = ({ pitch, ...rest }): ReactElement => (
   <div className={cn('kanban-pitch', rest.className)}>
     <p className="pitch-title">{pitch.title}</p>
-    <p>Due {pitch.deadline || '01/01/2022'}</p>
+    <div className="pitch-info">
+      <FieldTag size="small" content={pitch.editStatus} />
+      <p>Due {pitch.deadline || '01/01/2022'}</p>
+    </div>
   </div>
 );
 
