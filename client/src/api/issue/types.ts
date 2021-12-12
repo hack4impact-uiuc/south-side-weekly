f3bf1b227d7bf0f5fd65f35f78e58e4c5b447d8b
Aditya <adityajain1921@gmail.com>
2021-12-12 07:34:12 -0600

    Move to types

--- client/src/api/issue/types.ts
+++ client/src/api/issue/types.ts
@@ -1,4 +1,4 @@
-import { IIssue } from 'ssw-common';
+import { IIssue, IPitch } from 'ssw-common';
 
 import { ApiResponseBase } from '../types';
 export interface IssuesResponse extends ApiResponseBase {
@@ -6,4 +6,7 @@ export interface IssuesResponse extends ApiResponseBase {
 }
 export interface IssueResponse extends ApiResponseBase {
   result: IIssue;
+}
+export interface PitchBucketsResponse extends ApiResponseBase {
+  result: { status: string; pitches: IPitch[] }[];
 }
