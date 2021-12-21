31ec5fb162d3b4cd892245cc6d968c951bda62de
Aditya <adityajain1921@gmail.com>
2021-12-21 05:22:13 -0500

    Add missing api function

---
+++ client/src/api/userFeedback/index.ts
@@ -0,0 +1,54 @@
+import { IUserFeedback } from 'ssw-common';
+
+import { Response } from '../types';
+import { buildEndpoint, get, post, put } from '../builders';
+
+import { UserFeedbackResponse, UsersFeedbackResponse } from './types';
+
+const USER_FEEDBACK_ENDPOINT = '/userFeedback';
+
+const addFeedback = async (
+  newFeedback: Partial<IUserFeedback>,
+): Promise<Response<UserFeedbackResponse>> => {
+  const url = buildEndpoint(USER_FEEDBACK_ENDPOINT);
+  const failureMessage = 'ADD_USER_FEEDBACK_FAIL';
+
+  return await post(url, newFeedback, failureMessage);
+};
+
+const getUserFeedbackForPitch = async (
+  userId: string,
+  pitchId: string,
+): Promise<Response<UserFeedbackResponse>> => {
+  const url = buildEndpoint(USER_FEEDBACK_ENDPOINT, userId, pitchId);
+  const failureMessage = 'GET_USER_FEEDBACK_ON_PITCH_FAIL';
+
+  return await get(url, failureMessage);
+};
+
+// Updates the information on user feedback
+const updateUserFeedback = async (
+  userFeedbackData: Partial<IUserFeedback>,
+  userFeedbackId: string,
+): Promise<Response<UserFeedbackResponse>> => {
+  const url = buildEndpoint(USER_FEEDBACK_ENDPOINT, userFeedbackId);
+  const failureMessage = 'UPDATE_USER_FEEDBACK_FAIL';
+
+  return await put(url, userFeedbackData, failureMessage);
+};
+
+const getUserFeedback = async (
+  userId: string,
+): Promise<Response<UsersFeedbackResponse>> => {
+  const url = buildEndpoint(USER_FEEDBACK_ENDPOINT, 'user', userId);
+  const failureMessage = 'GET_FEEDBACK_FAIL';
+
+  return await get(url, failureMessage);
+};
+
+export {
+  addFeedback,
+  getUserFeedbackForPitch,
+  updateUserFeedback,
+  getUserFeedback,
+};
